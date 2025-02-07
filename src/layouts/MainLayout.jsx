2ff62fe6e7a3c982383cfaa87686b2e433e4dc28
Jacob <35972287+MazrimTaim93@users.noreply.github.com>
2025-02-06 18:18:33 -0700

    Main layout (#1)
    * added mainlayout
    
    * Layouts working properly

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import  { useEffect } from "react"
+import MainNav from "../pages/MainNav"
+import MainFooter from "../pages/MainFooter"
+
+const MainLayout = ({ children, title }) =>
+{
+    useEffect(() => {
+        if (title) {
+            document.title = title
+        }
+    }, [title])
+
+    return (
+        <div>
+            <MainNav />
+            {children}
+            <MainFooter />
+        </div>
+    )
+}
+
+export default MainLayout;
---
+++ src/pages/MainNav.jsx
@@ -0,0 +1,18 @@
+//import { Link } from "react"
+
+const MainNav = () => {
+    return (
+		<nav>
+			<ul>
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="/about">About Me</a>
+				</li>
+			</ul>
+		</nav>
+    )
+}
+
+export default MainNav
